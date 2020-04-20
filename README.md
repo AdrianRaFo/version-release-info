64e42d4d909f3a7e6ecfe586a0b4ecd75e731e0e
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-04-20 17:36:51 +0200

    clean and and document

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-A little github action to get information about the version from a properties file
+# Version-release-info
+
+A little github action to get information about the version format.
+
+## Output
+
+This action provides the following information:
+
+ - has-valid-format: If the version has the right format: major.minor.patch-M|RC|SNAPSHOT
+ 
+ - is-release: If the version has a release format. This excludes milestones, release-candidates and snapshots
+
+ - is-pre-release: If the version has a pre-release format like milestones or release-candidates
+
+ - is-snapshot: If the version is a snapshot.
+
+## Example
+
+```yaml
+- name: Get version
+  run: echo "::set-env name=git-version::$(git describe --abbrev=0 --tags)"
+uses: AdrianRaFo/version-release-info@v1
+  with:
+    version: env.git-version
+- name: Do something if release
+  if: steps.is-release == 'true'
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 
 #Args
 set -u
-VERSION_FILE=$1
-VERSION_KEY=$2
+VERSION=$1
 
 #Get version info
-VERSION_VALUE=$(cat "$VERSION_FILE" | grep "$VERSION_KEY" | cut -d'=' -f2 | tr -d '[:space:]')
+VERSION_VALUE=$(echo "$VERSION" | tr -d '[:space:]')
 
 if [[ "$VERSION_VALUE" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   IS_RELEASE=true
@@ -31,14 +30,7 @@ else
   HAS_VALID_FORMAT=false
 fi
 
-echo "::set-env name=VERSION_VALUE::$VERSION_VALUE"
-echo "::set-env name=IS_RELEASE::$IS_RELEASE"
-echo "::set-env name=IS_PRE_RELEASE::$IS_PRE_RELEASE"
-echo "::set-env name=IS_SNAPSHOT::$IS_SNAPSHOT"
-echo "::set-env name=HAS_VALID_FORMAT::$HAS_VALID_FORMAT"
-
-echo "::set-output name=version_value::$VERSION_VALUE"
-echo "::set-output name=is_release::$IS_RELEASE"
-echo "::set-output name=is_pre_release::$IS_PRE_RELEASE"
-echo "::set-output name=is_snapshot::$IS_SNAPSHOT"
-echo "::set-output name=has_valid_format::$HAS_VALID_FORMAT"
+echo "::set-output name=is-release::$IS_RELEASE"
+echo "::set-output name=is-pre-release::$IS_PRE_RELEASE"
+echo "::set-output name=is-snapshot::$IS_SNAPSHOT"
+echo "::set-output name=has-valid-format::$HAS_VALID_FORMAT"
