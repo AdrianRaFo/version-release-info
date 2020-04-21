48cd8264d226e0a5aefd41f0721112dd32744932
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-04-21 12:38:14 +0200

    change bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #Args
 set -u
@@ -31,7 +31,7 @@ else
   HAS_VALID_FORMAT=false
 fi
 
-echo "::set-output name=is-release::$IS_RELEASE"
-echo "::set-output name=is-pre-release::$IS_PRE_RELEASE"
-echo "::set-output name=is-snapshot::$IS_SNAPSHOT"
-echo "::set-output name=has-valid-format::$HAS_VALID_FORMAT"
+echo "is-release::$IS_RELEASE"
+echo "is-pre-release::$IS_PRE_RELEASE"
+echo "is-snapshot::$IS_SNAPSHOT"
+echo "has-valid-format::$HAS_VALID_FORMAT"
